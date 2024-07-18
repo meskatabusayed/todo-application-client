a20b4d50125a8af0375e5cc865a0011489a84037
meskatabusayed <meskatabusayed@gmail.com>
2024-07-18 23:06:41 +0530

    update method added

--- src/redux/api/api.ts
+++ src/redux/api/api.ts
@@ -1,33 +1,47 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-
-
 export const baseApi = createApi({
-    reducerPath : "baseApi", // "reducerPath" ai name ta kiser vitti te hobe
-    baseQuery : fetchBaseQuery({baseUrl : 'http://localhost:5000'}),
-    tagTypes : ["todo"],
-    endpoints : (builder) => ({
-        getTodos : builder.query({
-            query: (priority) => ({
-                url : "/tasks",
-                method : "GET",
-                params : {priority},
-            }),
-            providesTags : ["todo"],
-        }),
-        addTodo : builder.mutation({
-            query: (data) => {
-                console.log("inside mutation" , data);
-                return{
-                    url : "/task",
-                method : "POST",
-                body : data,
-                }
-            },
-            invalidatesTags : ["todo"],
-        }),
-        
+  reducerPath: "baseApi", // "reducerPath" ai name ta kiser vitti te hobe
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000" }),
+  tagTypes: ["todo"],
+  endpoints: (builder) => ({
+    getTodos: builder.query({
+      query: (priority) => {
+        const params = new URLSearchParams();
+        if(priority){
+            params.append("priority" , priority);
+        }
+        return {
+          url: "/tasks",
+          method: "GET",
+          
+        };
+      },
+      providesTags: ["todo"],
     }),
-})
+    addTodo: builder.mutation({
+      query: (data) => {
+        console.log("inside mutation", data);
+        return {
+          url: "/task",
+          method: "POST",
+          body: data,
+        };
+      },
+      invalidatesTags: ["todo"],
+    }),
+    updateTodo: builder.mutation({
+      query: (data) => {
+        console.log("inside mutation", data);
+        return {
+          url: "/task",
+          method: "put",
+          body: data,
+        };
+      },
+      invalidatesTags: ["todo"],
+    }),
+  }),
+});
 
-export const {useGetTodosQuery , useAddTodoMutation} = baseApi;
+export const { useGetTodosQuery, useAddTodoMutation } = baseApi;
